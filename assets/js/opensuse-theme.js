c8e61d7e472c22f4db00b560ab24792684b0db7c
eyea <deyesea@gmail.com>
2019-04-22 23:39:18 +0800

    basic change ui

--- assets/js/opensuse-theme.js
+++ assets/js/opensuse-theme.js
@@ -151,7 +151,7 @@ function osMoreInformation(os) {
   moreInfoOpened = true
   //get the height of the main container
   containerHeight = $('#opensuse-os').outerHeight();
-  
+
 
   //find the information of the selected distribution
   $osSelected = os
@@ -160,7 +160,7 @@ function osMoreInformation(os) {
   osSelectedIcon = $osSelected.find(".distributions-icon").html()
 
   window.location.hash = osSelectedTitle;
-  
+
   //dont let users click more than once
   $osSelected.addClass('not-clickable')
 
@@ -204,12 +204,12 @@ function osMoreInformation(os) {
     $('.os-icon').html(osSelectedIcon)
 
     $('.back-to-main-page').on('click', function() {
-      history.pushState('', document.title, window.location.pathname);  
+      history.pushState('', document.title, window.location.pathname);
       backToMainPageOs($osSelected)
     })
 
     $('#home, #opensuseLogo').on('click', function() {
-      history.pushState('', document.title, window.location.pathname);  
+      history.pushState('', document.title, window.location.pathname);
       scrolled = $(window).scrollTop();
       if (scrolled > mainSectionHeight) {
         setTimeout('backToMainPageOs($osSelected)', 900);
@@ -430,7 +430,7 @@ var contributionInterpolation = function () {
 
 // Add "openSUSE Linux OS" to the document Title
 $(document).on("ready", function() {
-  $("title").prepend('openSUSE - Linux OS. ')
+  $("title").prepend("eyea's website ~ ")
 })
 
 // check if SVG is supported by the user's browser
